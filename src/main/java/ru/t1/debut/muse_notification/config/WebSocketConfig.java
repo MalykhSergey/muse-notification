cf10496a49df3d12e169ccc5bddaa6706de1dd69
Malykh Sergey <sergeymalykhdm@gmail.com>
2025-08-21 15:03:30 +0600

    feat: Отказаться от SockJs

--- src/main/java/ru/t1/debut/muse_notification/config/WebSocketConfig.java
+++ src/main/java/ru/t1/debut/muse_notification/config/WebSocketConfig.java
@@ -26,8 +26,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setHandshakeHandler(handshakeHandler)
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
     }
 
 }
